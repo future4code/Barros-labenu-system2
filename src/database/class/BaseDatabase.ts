65071d4f1c5c17e9b7f7ca210536ce9bf851ea55
Giovana Inez Vieira <gioivieira@gmail.com>
2022-12-16 11:09:39 -0300

    Creation of ClassDatabase and of endpoints createClass and searchClass.

--- src/database/class/BaseDatabase.ts
+++ src/database/class/BaseDatabase.ts
@@ -1,10 +1,10 @@
-import knex from "knex"
+import knex, { Knex } from "knex"
 import dotenv from "dotenv"
 
 dotenv.config()
 
 export abstract class BaseDatabase {
-   protected static connection = knex({
+   static connection = knex({
       client: "mysql",
       connection: {
          host: process.env.DB_HOST,
@@ -20,12 +20,12 @@ export abstract class BaseDatabase {
    
    public async create(item: any) {
       await BaseDatabase.connection(this.TABLE_NAME).insert(item)
-   };
+   }
 
    public async getAll(column:string, like:string, value:string) {
       const result = await BaseDatabase.connection(this.TABLE_NAME).select().where(column, like, value)
       return result
-   };
+   }
 
    public async updateInfo(id: number, column: string, newValue: string) {
       await BaseDatabase.connection(this.TABLE_NAME).where({id}).update(column, newValue)
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import app from "./app";
 import { createStudent } from "./endpoints/createStudent";
 import { searchStudents } from "./endpoints/searchStudents";
 import { CreateInstructor } from "./endpoints/CreateInstructor";
+import { createClass } from "./endpoints/createClass";
+import { searchClass } from "./endpoints/searchClass";
 
 
 app.post("/students", createStudent)
@@ -9,3 +11,7 @@ app.get("/students", searchStudents)
 
 //Create instructor
 app.post("/instructors", CreateInstructor)
+
+app.post("/class/create", createClass)
+
+app.get("/class", searchClass)
